911aa160656cf003c9898a93cf58efd8e2b1fe48
Suraj Sharma <suraj200delhi@gmail.com>
2024-09-11 23:52:23 +0530

    fix backend isssue and create frontend still not complete

--- Week6_AUTH/Frontend/src/App.jsx
+++ Week6_AUTH/Frontend/src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Signin from './components/Signin'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Signin/>
+    <Navbar/>
+    <Home/>
     </>
   )
 }
