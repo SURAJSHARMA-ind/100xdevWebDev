03b467267318b0aebc798e6c634e54d461d4a2d9
Suraj Sharma <suraj200delhi@gmail.com>
2024-09-11 23:47:51 +0530

    Signin Component

--- Week6_AUTH/Frontend/src/components/SignIn.jsx
+++ Week6_AUTH/Frontend/src/components/SignIn.jsx
@@ -1,41 +1,60 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 
 function Signin() {
-    const [userdata,setUserData]=useState([])
+    const [request, setRequest] = useState('signup')
     const [formData, SetFormData] = useState({
         username: '',
-        email: '',
         password: ''
     })
+    const token = localStorage.getItem('token')
+    const handlesignin = () => {
+        setRequest('signin')
+    }
+    const handleprofile = () => {
+        setRequest('profile')
+    }
 
     const changeHandler = (e) => {
-        const {name,value} = e.target
-        SetFormData({...formData,[name]:value})
+        const { name, value } = e.target
+        SetFormData({ ...formData, [name]: value })
 
     }
-    const signupHandler =()=>{
-        
+    const signupHandler = async (e) => {
+        e.preventDefault()
+        const response = await axios.post(`http://localhost:5000/${request}`, formData, {
+            headers: {
+                Authorization: `${token}`
+            }
+        });
+        const settoken = response.data.token
+        localStorage.setItem('token', settoken)
+        console.log(response);
+        console.log(token);
     }
 
     return (
 
-        <form action='' method='post' className=" h-screen  bg-black w-full justify-center  flex flex-wrap items-center">
+        <form onSubmit={signupHandler} action='' method='post' className=" h-screen bg-gradient-to-tl from-zinc-900 to-slate-900 w-full justify-center  flex flex-wrap items-center">
+
             <div className=" justify-center p-4 bg-gray-100 rounded-lg  flex flex-col  min-w-[30%] ">
                 <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Sign In</h2>
                 <div className="relative mb-4">
                     <label for="UserName" className="leading-7 text-sm text-gray-600">User Name</label>
                     <input onChange={changeHandler} type="text" value={formData.username} name="username" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
-                <div className="relative mb-4">
+                {/* <div className="relative mb-4">
                     <label for="emial" className="leading-7 text-sm text-gray-600">Email</label>
                     <input onChange={changeHandler} type='email' value={formData.email} name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                </div>
+                </div> */}
                 <div className="relative mb-4">
                     <label for="password" className="leading-7 text-sm text-gray-600">Password</label>
                     <input onChange={changeHandler} type="password" value={formData.password} name="password" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
-                <button onClick={signupHandler} className="text-white bg-gray-800 border-0 py-2 px-8 focus:outline-none hover:bg-gray-900 rounded text-lg">Sign In</button>
+                <button className="text-white bg-gray-800 border-0 py-2 px-8 focus:outline-none hover:bg-gray-900 rounded text-lg">Sign up</button>
+                <button onClick={handlesignin} className="text-white bg-gray-800 border-0 py-2 px-8 focus:outline-none hover:bg-gray-900 rounded text-lg">Sign in</button>
+                <button onClick={handleprofile} className="text-white bg-gray-800 border-0 py-2 px-8 focus:outline-none hover:bg-gray-900 rounded text-lg">profile</button>
             </div>
         </form>
 
