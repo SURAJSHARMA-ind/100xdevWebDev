8f740911c819ed870d08d007fa5cfc8013ebb3c9
Suraj Sharma <suraj200delhi@gmail.com>
2024-09-11 23:47:12 +0530

    Home Component

---
+++ Week6_AUTH/Frontend/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className='h-screen bg-gradient-to-tl from-zinc-900 to-slate-900'>
+            <div className='flex justify-center item-center'>
+                <h1 className='text-4xl '>
+                    <span className='font-bold text-center bg-gradient-to-r from-sky-600 to-indigo-600 text-transparent bg-clip-text'>100XDevs,</span>
+                    <span className='font-bold text-center bg-gradient-to-r from-slate-300 to-slate-500 text-transparent bg-clip-text'>because</span>
+                    </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Home
